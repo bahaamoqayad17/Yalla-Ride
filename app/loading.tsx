6807378005237410e920c69759a1b77df7507d3c
bahaamoqayad17 <bahaa200030.m7taref@gmail.com>
2025-09-07 12:40:16 +0200

    Add Animation Loading Indicator

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="pl">
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__dot"></div>
+      <div className="pl__text">Loading…</div>
+    </div>
+  );
+}
